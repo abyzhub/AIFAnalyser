b9f489219eec2ceccad91ddf71b31391fdc162c8
abyzhub <aby.waits@gmail.com>
2013-10-31 00:06:47 +0530

    Resources Done

--- AifAnalyser/src/com/crmimpl/tool/XAifAnalyzeItemXB.java
+++ AifAnalyser/src/com/crmimpl/tool/XAifAnalyzeItemXB.java
@@ -2,8 +2,6 @@ package com.crmimpl.tool;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import com.amdocs.aif.consumer.ConsumerUtil;
 import com.amdocs.aif.consumer.ResourceDesc;
@@ -19,7 +17,6 @@ public class XAifAnalyzeItemXB extends XBeanImpl {
 
 	private ValueMapVo[] vm= null;
 
-	private ResourceDesc rd = null;
 
 	private ClassDetails[] elementArray = null;
 	
@@ -36,40 +33,32 @@ public class XAifAnalyzeItemXB extends XBeanImpl {
 	protected void preExecute(BoContext arg0) {
 		try {
 			if (item.contains("Type -> ")) {
-				ArrayList<ClassDetails> arl = new ArrayList<ClassDetails>();
-				String clazzName = item.replace("Type -> ", "");
-				if(clazzName.endsWith("[]")){
-					clazzName = clazzName.substring(0,clazzName.length()-2);
-				}
-				
-				Method[] methods = Class.forName(clazzName).getDeclaredMethods();
-				ClassDetails clsdtl = null;
-				for (int i = 0; i < methods.length; i++) {
-					String methodName = methods[i].getName();
-					String className = methods[i].getReturnType().getName();
-					if (methodName.startsWith("get")) {
-						clsdtl = new  ClassDetails();
-						clsdtl.setParamName(methodName.substring(3));
-						clsdtl.setParamType(className);
-						arl.add(clsdtl);
-					}
-					if (methods[i].getName().startsWith("is")) {
-						clsdtl = new  ClassDetails();
-						clsdtl.setParamName(methodName.substring(2));
-						clsdtl.setParamType(className);
-						arl.add(clsdtl);
-					}
-				}
-				elementArray =(ClassDetails[])arl.toArray( new ClassDetails[arl.size()]);
-			} else if (item.contains("ResourceName")) {
-
+				analyzeDatatype();
+			} else if (item.contains("Binding -> ")) {
 				if (getBindingType() != -1) {
-					String str = item.replace("ResourceName -> ", "");
-					rd = ConsumerUtil.getResourceDesc(str,
-							BindingType.valueOf(getBindingType()));
-
+					BindingType bindingType =BindingType.valueOf(getBindingType()); 
+					ResourceDesc rd = ConsumerUtil.getResourceDesc(getResourceName(), bindingType);
+					if (bindingType == BindingType.EJB) {
+						String configJndi = rd.getResourceNode().getString(
+								"JndiHome", "");
+						elementArray = new ClassDetails[5];
+						elementArray[0]=new ClassDetails();
+						elementArray[0].setParamName("JNDI");
+						elementArray[0].setParamType(configJndi);
+						elementArray[1]=new ClassDetails();
+						elementArray[1].setParamName("Method");
+						elementArray[1].setParamType("");
+						elementArray[2]=new ClassDetails();
+						elementArray[2].setParamName("Remote Class");
+						elementArray[2].setParamType("");
+						elementArray[3]=new ClassDetails();
+						elementArray[3].setParamName("Home Class");
+						elementArray[3].setParamType("");
+						elementArray[4]=new ClassDetails();
+						elementArray[4].setParamName("Local ");
+						elementArray[4].setParamType("");
+					}
 				}
-
 			}
 		} catch (Exception e) {
 			throw new UifDisplayableException("Error: "
@@ -78,15 +67,53 @@ public class XAifAnalyzeItemXB extends XBeanImpl {
 
 	}
 
+	private void analyzeDatatype() throws ClassNotFoundException {
+		ArrayList<ClassDetails> arl = new ArrayList<ClassDetails>();
+		String clazzName = item.replace("Type -> ", "");
+		if(clazzName.endsWith("[]")){
+			clazzName = clazzName.substring(0,clazzName.length()-2);
+		}
+		
+		Method[] methods = Class.forName(clazzName).getDeclaredMethods();
+		ClassDetails clsdtl = null;
+		for (int i = 0; i < methods.length; i++) {
+			String methodName = methods[i].getName();
+			String className = methods[i].getReturnType().getName();
+			if (methodName.startsWith("get") && methods[i].getParameterTypes().length==0) {
+				if(methods[i].getReturnType().isArray())
+					className = className.substring(2, className.length()-1) + "[]";
+				clsdtl = new  ClassDetails();
+				clsdtl.setParamName(methodName.substring(3));
+				clsdtl.setParamType(className);
+				arl.add(clsdtl);
+			}
+			if (methods[i].getName().startsWith("is")) {
+				clsdtl = new  ClassDetails();
+				clsdtl.setParamName(methodName.substring(2));
+				clsdtl.setParamType(className);
+				arl.add(clsdtl);
+			}
+		}
+		elementArray =(ClassDetails[])arl.toArray( new ClassDetails[arl.size()]);
+	}
+
 	private int getBindingType() {
 		for (int i = 0; i < vm.length; i++) {
-			String str = item.replace("ResourceName -> ", "");
-			if (vm[i].getParentFldVal().equals(str))
+			if (vm[i].getParentFldVal().equals("Binding"))
 			{
 				return Integer.parseInt(vm[i].getChildFldVal());
 			}
 		}
 		return -1;
+	}
+	private String getResourceName() {
+		for (int i = 0; i < vm.length; i++) {
+			if (vm[i].getParentFldVal().equals("Resource"))
+			{
+				return vm[i].getChildFldVal();
+			}
+		}
+		return "";
 	}
 
 	public void setItem(String item) {
@@ -97,9 +124,6 @@ public class XAifAnalyzeItemXB extends XBeanImpl {
 		this.vm = vmap;
 	}
 
-	public ResourceDesc getRd() {
-		return rd;
-	}
 	public ClassDetails[] getElementArray() {
 		return elementArray;
 	}
